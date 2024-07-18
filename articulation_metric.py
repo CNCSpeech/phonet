c5046251c5aefeb2a5915359f5df66c8f7259a69
asosawelford <alejandrososawelford@gmail.com>
2024-07-18 10:32:50 -0300

    Refactor code to improve phonological scoring and add trained models

---
+++ articulation_metric.py
@@ -0,0 +1,41 @@
+import os
+import numpy as np
+import pandas as pd
+
+def articulation_metric(csv_file):
+    phonological_classes = pd.read_csv(csv_file)
+    target_plosives = phonological_classes['target_plosives']
+    non_target_plosives = phonological_classes['non_target_plosives']
+
+    # Convert target and non-target plosives to numpy arrays
+    target_plosives = np.array(target_plosives)
+    non_target_plosives = np.array(non_target_plosives)
+
+    # Calculate accuracy for target and non-target plosives
+    target_accuracy = np.mean(target_plosives)
+    non_target_accuracy = np.mean(non_target_plosives)
+
+    return target_accuracy, non_target_accuracy
+
+if __name__ == "__main__":
+    # Initialize an empty DataFrame
+    df = pd.DataFrame(columns=['filename', 'target_accuracy', 'non_target_accuracy'])
+
+    # Path to the folder containing CSV files
+    csv_folder = "phonet/fondecyt_pataka_results/"
+    for csv_file in os.listdir(csv_folder):
+        if csv_file.endswith(".csv"):
+            csv_file_path = os.path.join(csv_folder, csv_file)
+            target_accuracy, non_target_accuracy = articulation_metric(csv_file_path)
+            filename = os.path.basename(csv_file_path)
+            new_row = pd.DataFrame({"filename": [filename], "target_accuracy": [target_accuracy], "non_target_accuracy": [non_target_accuracy]})
+            df = pd.concat([df, new_row], ignore_index=True)
+    
+    # Save the results to a new CSV file
+    df.to_csv("phonet/fondecyt_pataka_scores.csv", index=False)
+
+
+    
+
+    # Save the results to a new CSV file
+    df.to_csv("phonet/fondecyt_pataka_scores_mean.csv", index=False)
--- phonet/test_phonet.py
+++ phonet/test_phonet.py
@@ -18,49 +18,16 @@ if __name__=="__main__":
 
     if not os.path.exists(path_results):
         os.makedirs(path_results)
-    ## get the "stop" phonological posterior from a single file
-    file_audio=PATH+"/audios/pataka.wav"
-    file_feat=path_results+"/pataka"
-    phon=Phonet(["target_plosives", "non_target_plosives", "target_vowels", "vocalic"])
-    phon.get_phon_wav(file_audio, file_feat, True)
+
+    # file_audio=PATH+"/audios/pataka.wav"
+    # file_feat=path_results+"/pataka"
+    # phon=Phonet(["target_plosives", "non_target_plosives", "target_vowels", "vocalic"])
+    # phon.get_phon_wav(file_audio, file_feat, True)
 
     # get phonological posteriors from de audio files included in a directory
-    directory=PATH+"/phonclasses/"
     phon=Phonet(["target_plosives", "non_target_plosives", "target_vowels", "vocalic"])
-    phon.get_phon_path(PATH+"/audios/", path_results, True)
+    phon.get_phon_path(PATH+"/audios/", path_results, plot_flag = True)
 
     # compute the posteriorgram for an audio_file for different phonological posteriors
-    phon=Phonet(["target_plosives", "non_target_plosives", "target_vowels", "vocalic"])
-    phon.get_posteriorgram(file_audio)
-
-    # get the "nasal" phonological posterior from a single file
-    file_audio=PATH+"/audios/sentence.wav"
-    file_feat=path_results+"/sentence_nasal"
-    phon=Phonet(["nasal"])
-    # phon.get_phon_wav(file_audio, file_feat, False)
-
-    # get the "strident" phonological posterior from a single file
-    file_feat=path_results+"/sentence_strident"
-    phon=Phonet(["strident"])
-    # phon.get_phon_wav(file_audio, file_feat)
-
-    # get "strident, nasal, and back" phonological posteriors from a single file
-    file_feat=path_results+"/sentence_all"
-    phon=Phonet(["strident", "nasal", "back"])
-    # phon.get_phon_wav(file_audio, file_feat)
-
-
-    # compute the posteriorgram for an audio_file for different phonological posteriors
-    phon=Phonet(["vocalic", "strident", "nasal", "back", "stop", "pause"])
-    phon.get_posteriorgram(file_audio)
-
-
-    # get phonological posteriors from de audio files included in a directory
-    directory=PATH+"/phonclasses/"
-    phon=Phonet(["vocalic", "strident", "nasal", "back", "stop", "pause"])
-    phon.get_phon_path(PATH+"/audios/", path_results)
-
-    ## get the PLLR features from an audio file
-    # phon=Phonet(["all"])
-    # PLLR=phon.get_PLLR(file_audio)
-    # print(PLLR.head())
+    # phon=Phonet(["target_plosives", "non_target_plosives", "target_vowels", "vocalic"])
+    # phon.get_posteriorgram(file_audio)
